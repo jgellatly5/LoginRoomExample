59c8130797599c69bc465c1beb7324a2d47f7ae3
jgellatly5 <jgellatly5@gmail.com>
2020-08-05 08:48:51 -0400

    initial commit

---
+++ app/src/main/java/com/jordangellatly/roompracticejuly29/WordRepository.kt
@@ -0,0 +1,12 @@
+package com.jordangellatly.roompracticejuly29
+
+import androidx.lifecycle.LiveData
+
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
